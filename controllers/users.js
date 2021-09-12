98e1d6d9df552e5fcefbfde36c74f696d11891c8
Иван Галайдо <75268958+Ivan-Galaido@users.noreply.github.com>
2021-09-12 22:01:23 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -48,7 +48,7 @@ const login = (req, res, next) => {
         httpOnly: true,
       }).send(user);
     })
-    .catch((err) => next(new Unauthorized('err.message')));
+    .catch((err) => next(new Unauthorized('Ошибка авторизации')));
 };
 const logout = (req, res) => {
   res.clearCookie('jwt').send({ success: true });
