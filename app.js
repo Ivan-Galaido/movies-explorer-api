d9a983ec95ef6953f52edbc38400a738b7605029
Иван Галайдо <75268958+Ivan-Galaido@users.noreply.github.com>
2021-10-15 23:12:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(cors({
   exposedHeaders: '*',
   credentials: true,
 }));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://praktikum.tk');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
