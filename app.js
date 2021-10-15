428930bd1df03a19ebbdeaca66708cb0791b9d9d
Иван Галайдо <75268958+Ivan-Galaido@users.noreply.github.com>
2021-10-16 00:04:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -24,11 +24,20 @@ mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
-app.use(cors({
-  origin: true,
-  exposedHeaders: '*',
-  credentials: true,
-}));
+const allowedCors = [
+  'https://api.movies-ex.students.nomoredomains.club',
+  'http://movies-ex.nomoredomains.club',
+  'localhost:3000'
+];
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers; 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+}); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
