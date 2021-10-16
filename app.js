2b92b4557c6105a9d18cfef624ccb0e04b4e889f
Иван Галайдо <75268958+Ivan-Galaido@users.noreply.github.com>
2021-10-16 12:25:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,22 +6,14 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
-const { limiter, allowedCors } = require('./config');
+const { limiter, corsOptions } = require('./config');
 const routes = require('./routes');
 const errorHeandler = require('./middlewares/error-heandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
 const app = express();
-app.use(function(req, res, next) {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
-}); 
+ 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -32,6 +24,7 @@ mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
