1acb6e3b66210fd6434f72e1282dccd3e287beb3
Иван Галайдо <75268958+Ivan-Galaido@users.noreply.github.com>
2021-10-20 18:40:41 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,11 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
-const AuthError = require('../errors/auth-err');
+const Unauthorized = require('../errors/unauthorized');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизиция');
+    throw new Unauthorized('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new AuthError('Необходима авторизиция');
+    throw new Unauthorized('Необходима авторизация');
   }
 
   req.user = payload;
